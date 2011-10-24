2938f1e3943e478ef516f6be22b89209df3ed3fd
John Huddleston <huddlej@gmail.com>
2011-10-23 19:08:19 -0700

    added docstring for CsvFile model.

--- csv_admin/models.py
+++ csv_admin/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 
 class CsvFile(models.Model):
+    """
+    Associated an uploaded CSV file with a Django content type.
+
+    The relationship between the file and the content type is used to look up an
+    admin form for that content type. Forms validate CSV data and import the
+    validated data into Django models.
+    """
     csv = models.FileField(upload_to="csv_admin")
     content_type = models.ForeignKey(ContentType)
     added_on = models.DateTimeField(auto_now_add=True)
