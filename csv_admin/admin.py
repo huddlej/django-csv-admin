9d8aae74dd1eae9db6b3de3ad7cf861090366239
John Huddleston <huddlej@gmail.com>
2011-10-23 17:50:41 -0700

    added list display settings for CsvFile in django admin.

--- csv_admin/admin.py
+++ csv_admin/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from models import CsvFile
 
 
-admin.site.register(CsvFile)
+class CsvFileAdmin(admin.ModelAdmin):
+    list_display = ("csv", "added_on")
+
+admin.site.register(CsvFile, CsvFileAdmin)
