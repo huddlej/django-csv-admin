97c0b957333ae40648d89fb0e95278ca6bac5d00
John Huddleston <huddlej@gmail.com>
2011-10-27 20:48:06 -0700

    added a unicode method for the CsvFile model.

--- csv_admin/models.py
+++ csv_admin/models.py
@@ -13,3 +13,6 @@ class CsvFile(models.Model):
     csv = models.FileField(upload_to="csv_admin")
     content_type = models.ForeignKey(ContentType)
     added_on = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.csv.name
